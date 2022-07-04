dd7d5342ef386c927b78f559762a8c8cd50294c3
ChuaQiuLi <qiulichua@gmail.com>
2022-07-04 13:00:44 +0800

    initial commit

--- web/test.php
+++ web/test.php
@@ -1,19 +1,15 @@
-<html lang = "en">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-
-<title>Test</title>
-
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="width=device-width, initial-scale=1.0">
-<meta charset = "UTF-8">
-
-
+    <title>Test</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
 </head>
 <body>
-
-    
-  <h1>Hello World! </h1>
+  Hello World!
 
 </body>
-</html>
 
+</html>
